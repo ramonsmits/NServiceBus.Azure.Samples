7ee561308dd8a5a8ad8e490a056439311d691748
Yves Goeleven <yves@goeleven.com>
2013-11-07 17:30:01 +0100

    tested upgrade path from SDK 2.1 to 2.2 with latest nsb

--- VideoStore.AzureStorageQueues.Cloud/VideoStore.Operations/WorkerRole.cs
+++ VideoStore.AzureStorageQueues.Cloud/VideoStore.Operations/WorkerRole.cs
@@ -1,11 +1,24 @@
+using Microsoft.WindowsAzure.ServiceRuntime;
 using NServiceBus.Hosting.Azure;
 
 namespace VideoStore.Operations
 {
-    // Note: We're using NServiceBus.Hosting.Azure.RoleEntryPoint as a base class
-
     public class WorkerRole : RoleEntryPoint
     {
+        private NServiceBusRoleEntrypoint nsb = new NServiceBusRoleEntrypoint();
 
+        public override bool OnStart()
+        {
+            nsb.Start();
+
+            return base.OnStart();
+        }
+
+        public override void OnStop()
+        {
+            nsb.Stop();
+
+            base.OnStop();
+        }
     }
 }
