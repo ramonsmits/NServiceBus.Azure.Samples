ef747daf1908c804b91616f2e6f0f5f37dfe4876
Yves Goeleven <yves@goeleven.com>
2014-03-17 11:04:48 +0100

    Experimental azure servicebus channel for NServiceBus.Connect

---
+++ NServiceBus.Connect.Experimental/SiteA/EndpointConfig.cs
@@ -0,0 +1,22 @@
+﻿using NServiceBus.Features;
+
+namespace SiteA
+{
+    using NServiceBus;
+
+    // The endpoint is started with the RunGateway profile which turns it on. The Lite profile is also
+    // active which will configure the persistence to be InMemory
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server,IWantCustomInitialization
+    {
+        public void Init()
+        {
+            Feature.Disable<NServiceBus.Features.Gateway>();
+            Feature.Enable<NServiceBus.Connect.Features.Gateway>();
+
+            Configure.With()
+                .DefaultBuilder()
+                .AzureDataBus();
+               // .FileShareDataBus(".\\databus");
+        }
+    }
+}
