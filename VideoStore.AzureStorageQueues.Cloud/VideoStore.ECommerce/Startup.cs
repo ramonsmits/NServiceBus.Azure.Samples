36eb50591e87fd7c66864e2d52e4a4a6b061b011
Brian Buvinghausen <brian@buvinghausen.com>
2014-03-19 07:02:54 -0500

    Upgrade Azure Storage Queues to latest of everything

---
+++ VideoStore.AzureStorageQueues.Cloud/VideoStore.ECommerce/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using VideoStore.ECommerce;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace VideoStore.ECommerce
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			app.MapSignalR();
+		}
+	}
+}
