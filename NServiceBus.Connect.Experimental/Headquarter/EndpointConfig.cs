bd3514a1503625b392d7c5c70517ebbb99500c7f
Yves Goeleven <yves@goeleven.com>
2014-04-09 13:50:16 +0200

    Revert "removing experimental connect sample"
    This reverts commit 7efb26869673bb72f8085b3481ad02743e43b7eb.

---
+++ NServiceBus.Connect.Experimental/Headquarter/EndpointConfig.cs
@@ -0,0 +1,27 @@
+﻿using NServiceBus.Features;
+
+namespace Headquarter
+{
+    using NServiceBus;
+
+    // The endpoint is started with the RunGateway profile which turns it on. The Lite profile is also
+    // active which will configure the persistence to be InMemory
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server,IWantCustomInitialization
+    {
+        public void Init()
+        {
+            Feature.Disable<NServiceBus.Features.Gateway>();
+            Feature.Enable<NServiceBus.Connect.Features.Gateway>();
+            
+            //Feature.Enable<NServiceBus.Gateway.V2.Features.Gateway>(c => c.);
+            
+            //Configure.Features.Gateway(c => c);
+
+            Configure.With()
+                .DefaultBuilder()
+                .AzureDataBus();
+            // .FileShareDataBus(".\\databus");
+        }
+    }
+
+}
