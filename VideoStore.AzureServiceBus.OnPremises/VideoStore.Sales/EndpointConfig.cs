ab332fc4d051d5d6c433960119a063e8a440e13e
Yves Goeleven <yves@goeleven.com>
2014-06-19 13:01:13 +0200

    improved configuration

--- VideoStore.AzureServiceBus.OnPremises/VideoStore.Sales/EndpointConfig.cs
+++ VideoStore.AzureServiceBus.OnPremises/VideoStore.Sales/EndpointConfig.cs
@@ -5,32 +5,15 @@ namespace VideoStore.Sales
     using System;
     using NServiceBus;
 
-    public class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher, UsingTransport<AzureServiceBus>, IWantCustomInitialization
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher, UsingTransport<AzureServiceBus>
     {
-        public Configure Init()
+        public void Customize(ConfigurationBuilder builder)
         {
-            var assemblyScanner = new AssemblyScanner();
-            assemblyScanner.MustReferenceAtLeastOneAssembly.Add(typeof(IHandleMessages<>).Assembly);
-            var assembliesToScan = assemblyScanner
-                .GetScannableAssemblies()
-                .Assemblies;
-
-            var endpointName = this.GetType().Namespace ?? this.GetType().Assembly.GetName().Name;
-            //endpointVersionToUse = FileVersionRetriever.GetFileVersion(specifier.GetType());
-
-            var config = Configure.With(o =>
-            {
-                o.EndpointName(endpointName);
-                // o.EndpointVersion(() => endpointVersionToUse);
-                o.AssembliesToScan(assembliesToScan);
-                o.Conventions(c =>
-                        c.DefiningCommandsAs(t => t.Namespace != null && t.Namespace.StartsWith("VideoStore") && t.Namespace.EndsWith("Commands"))
-                         .DefiningEventsAs(t => t.Namespace != null && t.Namespace.StartsWith("VideoStore") && t.Namespace.EndsWith("Events"))
-                         .DefiningMessagesAs(t => t.Namespace != null && t.Namespace.StartsWith("VideoStore") && t.Namespace.EndsWith("RequestResponse"))
-                         .DefiningEncryptedPropertiesAs(p => p.Name.StartsWith("Encrypted")));
-            });
-
-            return config;
+            builder.Conventions(c =>
+                c.DefiningCommandsAs(t => t.Namespace != null && t.Namespace.StartsWith("VideoStore") && t.Namespace.EndsWith("Commands"))
+                 .DefiningEventsAs(t => t.Namespace != null && t.Namespace.StartsWith("VideoStore") && t.Namespace.EndsWith("Events"))
+                 .DefiningMessagesAs(t => t.Namespace != null && t.Namespace.StartsWith("VideoStore") && t.Namespace.EndsWith("RequestResponse"))
+                 .DefiningEncryptedPropertiesAs(p => p.Name.StartsWith("Encrypted")));
         }
     }
 
